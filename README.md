6852a0e93660c25a7759b8a9cbd93a31e5407e5a
Janez Justin <janez.justin0@gmail.com>
2021-04-09 03:33:06 +0200

    ADD mouth void

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # photo-stretcher
 
 https://github.com/codeniko/shape_predictor_81_face_landmarks  
-https://github.com/italojs/facial-landmarks-recognition
+https://github.com/italojs/facial-landmarks-recognition  
+https://github.com/mauckc/mouth-open
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+from scipy.spatial import distance as dist
 import dlib
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,6 +8,8 @@ from tqdm import tqdm
 from math import floor
 
 N_OF_LANDMARKS = 68
+MOUTH_AR_THRESH = 0.79
+
 predictor = dlib.shape_predictor(
     f"shape_predictor_{N_OF_LANDMARKS}_face_landmarks.dat")
 detector = dlib.get_frontal_face_detector()
@@ -35,16 +38,17 @@ class Image:
         return (self.cols, self.rows)
 
     # draws face detection data on img
-    def draw(self):
+    def draw(self, points=None, face=None):
+        if points is None:
+            points = self.points
+        if face is None:
+            face = self.face
         img = self.img.copy()
-        cv2.rectangle(img=img, pt1=(self.face.left(), self.face.top()), pt2=(
-            self.face.right(), self.face.bottom()), color=(0, 0, 255), thickness=4)
-        for n in range(0, N_OF_LANDMARKS):
-            x = self.landmarks.part(n).x
-            y = self.landmarks.part(n).y
-
+        cv2.rectangle(img=img, pt1=(face.left(), face.top()), pt2=(
+            face.right(), face.bottom()), color=(0, 0, 255), thickness=4)
+        for [x, y] in points:
             # Draw a circle
-            cv2.circle(img=img, center=(x, y), radius=3,
+            cv2.circle(img=img, center=(floor(x), floor(y)), radius=3,
                        color=(0, 255, 0), thickness=-1)
     # [:,:,::-1] converts to RGB color space
         return img
@@ -60,6 +64,7 @@ class Image:
         if not self.contains_face:
             return self.add_text("No face found", fontColor=(255, 0, 0))
 
+        img = self.img.copy()
         # face's center
         fx1, fy1 = from_img.points[27][0], from_img.points[27][1]
         ax1, ay1 = anchor.points[27][0], anchor.points[27][1]
@@ -70,6 +75,15 @@ class Image:
                    for i in range(N_OF_LANDMARKS)]
 
         from_pts = from_img.points.copy()
+
+        # handle mouth
+        A = dist.euclidean(from_pts[51], from_pts[59])
+        B = dist.euclidean(from_pts[53], from_pts[57])
+        C = dist.euclidean(from_pts[49], from_pts[55])
+
+        # compute the mouth aspect ratio
+        mar = (A + B) / (2.0 * C)
+
         # face's coordinates
         tx1, ty1 = self.face.left(), self.face.top()
         tx2, ty2 = self.face.right(), self.face.bottom()
@@ -82,12 +96,17 @@ class Image:
             from_pts[i][0] = self.points[i][0] + x_scale*changes[i][0]  # x
             from_pts[i][1] = self.points[i][1] + y_scale*changes[i][1]  # y
 
+
         tform = PiecewiseAffineTransform()
         tform.estimate(np.float32(from_pts+self.border),  # border has to be self, so that image does not compress
                        np.float32(self.points + self.border))
 
-        return warp(self.img, tform, output_shape=(self.rows, self.cols))
-
+        img = warp(img, tform, output_shape=(self.rows, self.cols))
+        # overwrite mouth
+        if(mar > MOUTH_AR_THRESH):
+            pts = np.int32(from_pts[60:68])
+            cv2.fillPoly(img, [pts], (255, 255, 255))
+        return img
 
 class Video:
     def __init__(self, vid):
@@ -112,7 +131,7 @@ if __name__ == "__main__":
     to_img = Image(cv2.imread("to2.jpg"))
     print("done loading")
 
-    fps = 12
+    fps = 6
     out = cv2.VideoWriter(
         "ouput.mp4", cv2.VideoWriter_fourcc(*'MP4V'), fps, to_img.size())
 
